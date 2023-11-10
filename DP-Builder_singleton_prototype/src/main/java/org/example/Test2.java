12cc7ce2c4563c9af703ba9b54e3556e227f455d
AKASMIOU Ouassima <o.akasmiou@etu.enset-media.ac.ma>
2023-11-10 21:20:49 +0100

    builder singleton prototype

---
+++ DP-Builder_singleton_prototype/src/main/java/org/example/Test2.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.example.enums.AccountStatus;
+import org.example.enums.AccountType;
+import org.example.model.BankAccount;
+import org.example.model.Customer;
+
+public class Test2 {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        BankAccount account1=new BankAccount();
+        account1.setAccountId(1L);
+        account1.setCurrency("MAD");
+        account1.setType(AccountType.CURRENT_ACCOUNT);
+        account1.setBalance(10000);
+        account1.setStatus(AccountStatus.ACTIVATED);
+        account1.setCustomer(new Customer(1L, "ouassima"));
+
+
+
+         BankAccount account3=account1.clone();
+
+        System.out.println(account1);
+
+        System.out.println(account3);
+
+        System.out.println("******************************");
+
+        account1.setBalance(50000);
+        System.out.println(account1.getBalance());
+        System.out.println(account3.getBalance());
+
+        account1.getCustomer().setName("ouassima");
+        System.out.println(account1);
+
+        System.out.println(account3);
+    }
+}
